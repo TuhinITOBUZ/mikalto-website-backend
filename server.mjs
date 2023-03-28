2b9db9119420f5cc9745e74210df6ae05448106e
Tuhin Mukherjee <tuhin@itobuz.com>
2023-03-28 11:24:07 +0530

    backend update

--- server.mjs
+++ server.mjs
@@ -28,28 +28,19 @@ const server = http.createServer((req, res) => {
   try {
     res.setHeader("Access-Control-Allow-Origin", "*");
     res.setHeader("Access-Control-Allow-Headers", "*");
-    const chunks = [];
-    req.on("data", (chunk) => {
-      chunks.push(chunk);
+    const formData = [];
+    req.on("data", (formDataPeices) => {
+      formData.push(formDataPeices);
     });
     req.on("end", () => {
-      const data = Buffer.concat(chunks).toString();
-      console.log(data);
-      // if (data.length > 0) {
-      //   let list = [];
-      //   async function readFile() {
-      //     const data = await fs.readFile("./formData.txt", "utf8");
-      //     list.push(JSON.parse(data));
-      //     console.log(list);
-      //   }
-      //   readFile();
-      //   list.push(data)
-      //   console.log(list);
-      //   async function writeFile() {
-      //     await fs.writeFile("./formData.txt", list);
-      //   }
-      //   writeFile();
-      // }
+      let totalFormData = Buffer.concat(formData).toString();
+      if (totalFormData.length > 0) {
+        (async function () {
+          const dataFromFile = await fs.readFile("./formData.txt", "utf8");
+          totalFormData += ";" + dataFromFile;
+          await fs.writeFile("./formData.txt", totalFormData);
+        })();
+      }
     });
     res.end(getRequestData(req));
   } catch (err) {
