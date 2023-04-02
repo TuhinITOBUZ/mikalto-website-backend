a4632ba18764b7845d12feb7b968b50564915a0b
Tuhin Mukherjee <tuhinmuherjee74@gmail.com>
2023-04-02 19:15:01 +0530

    update

--- data.mjs
+++ data.mjs
@@ -1,63 +1,23 @@
-export const pageBanner = "https://i.ibb.co/7jdCPws/banner-Banner.jpg";
+export const pageBanner = {
+  url: "https://indiachalk.com/wp-content/uploads/2022/02/Untitled-1200-%C3%97-628-px-6-1200x628.png",
+  heading: "Enjoy your dream vaccation",
+  details: "Quite and endlessly beautiful hideaway",
+};
 
 export const homestays = {
+  heading: "WELCOME TO",
+  subHeading: "The Mikalto Luxury & Comfort Homestay",
+  details:
+    "welcome to our five-star hotel in Puerto de la Cruz's old town. A marvellous combination of architecture, design and contemporary art. 32 room and 10 uniquely different suites ensure our guests' stay is extra special.",
+  signature: "https://i.ibb.co/tJJTsCK/sign.jpg",
   homestay1: "https://i.ibb.co/K7khbYz/homestay1.jpg",
   homestay2: "https://i.ibb.co/GknZN9C/homestay2.jpg",
   homestay3: "https://i.ibb.co/y5cGBhb/homestay3.jpg",
 };
 
-export const locationSection = {
-  flowerLogo: "https://i.ibb.co/VQ4Gg48/flower.jpg",
-  busLogo: "https://i.ibb.co/RjsmpTw/bus.jpg",
-  vanLogo: "https://i.ibb.co/1Q7JRFW/van.jpg",
-  planeLogo: "https://i.ibb.co/bJHw5qg/plane.jpg",
-  largeTravelPicture: "https://i.ibb.co/TW3WNt9/travel-Image1.jpg",
-  smallTravelPicture1: "https://i.ibb.co/S008SJS/travel-Image2.jpg",
-  smallTravelPicture2: "https://i.ibb.co/MgfV0Zd/travel-Image3.jpg",
-};
-
-export const servicesBanner = "https://i.ibb.co/dBKh7XR/banner.jpg";
-
-export const activities = {
-  poolActivity: "https://i.ibb.co/hBXjhRs/pool.jpg",
-  forestHomestayActivity: "https://i.ibb.co/QXtygJJ/forest-Homestay.jpg",
-};
-
-export const wellnessService = {
-  url: "https://i.ibb.co/xYCYXrH/wellness.jpg",
-  heading: "WELLNESS",
-  subHeading: "Dedicated to wellness",
-  details:
-    " Aman's approach to wellness is focused on personalisation , privacy and seclusion. Local spa rituals with indigenous ingredients, offer a profoundly therapeutic touch, and a gentle sense of discovery.",
-};
-
-export const giftCardService = {
-  url: "https://i.ibb.co/2NwHZBM/giftCard.jpg",
-  heading: "GIFT CARDS",
-  subHeading: "The gift of future travel",
-  details:
-    "Share extraordinary experiences with the Aman E-Gift Card - delivery instantly and offering memories that last a lifetime",
-};
-
-export const spaService = {
-  url: "https://i.ibb.co/7vjXM6D/spa.jpg",
-  heading: "MIKALTO SHOP",
-  subHeading: "The spa experience from home",
-  details:
-    "Skincare and spa supplements harness powerful natural ingredients to provide balance and optimal welbeing.",
-};
-
-export const adventureService = {
-  url: "https://i.ibb.co/Z8W61M1/adventure.jpg",
-  heading: "EXPERIENCE",
-  subHeading: "Adventure in the wild",
-  details:
-    "Skincare and spa supplements harness powerful natural ingredients to provide balance and optimal welbeing.",
-};
-
 export const showRooms = {
   standardRoom: {
-    url: "https://i.ibb.co/WNrz0C5/standard-Room.jpg",
+    url: "https://images.unsplash.com/photo-1631049307264-da0ec9d70304?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8aG90ZWwlMjByb29tfGVufDB8fDB8fA%3D%3D&w=1000&q=80",
     heading: "STANDARD ROOM",
     price: "From: $150 / night",
     details:
@@ -68,7 +28,7 @@ export const showRooms = {
     hotelView: "garden view",
   },
   dulexRoom: {
-    url: "https://i.ibb.co/fG1M7XP/dulex-Room.jpg",
+    url: "https://cdn.dirigible.studio/wp-content/uploads/sites/84/2016/03/22124727/Presidential_Suite_Bedroom_NEW.jpg.webp",
     heading: "DELUXE ROOM",
     price: "From: $200 / night",
     details:
@@ -79,7 +39,7 @@ export const showRooms = {
     hotelView: "city view",
   },
   superDulexRoom: {
-    url: "https://i.ibb.co/p12W1wV/super-Dulex-Room.jpg",
+    url: "https://www.gannett-cdn.com/-mm-/05b227ad5b8ad4e9dcb53af4f31d7fbdb7fa901b/c=0-64-2119-1259/local/-/media/USATODAY/USATODAY/2014/08/13/1407953244000-177513283.jpg",
     heading: "SUPER DELUXE ROOM",
     price: "From: $300 / night",
     details:
@@ -91,55 +51,130 @@ export const showRooms = {
   },
 };
 
-export const text = {
-  homestayAdd: {
-    heading: "WELCOME TO",
-    subHeading: "The Mikalto Luxury & Comfort Homestay",
-    details: "welcome to our five-star hotel in Puerto de la Cruz's old town. A marvellous combination of architecture, design and contemporary art. 32 room and 10 uniquely different suites ensure our guests' stay is extra special.",
-    signature: "https://i.ibb.co/tJJTsCK/sign.jpg",
+export const ourHostelServices = {
+  service1: {
+    url: "https://i.ibb.co/YW0f0hQ/unique-Cuisine.jpg",
+    heading: "Unique cuisine",
+    details:
+      "We use the best ingredients to make special and really tasty dihes. Choose what you want to eat every day. You order at the moment.",
   },
-  ourHostelServices: {
-    service1: {
-      url: "https://i.ibb.co/YW0f0hQ/unique-Cuisine.jpg",
-      heading: "Unique cuisine",
-      details: "We use the best ingredients to make special and really tasty dihes. Choose what you want to eat every day. You order at the moment."
-    },
-    service2: {
-      url: "https://i.ibb.co/VqPmM5n/yoga.jpg",
-      heading: "Yoga & Gym",
-      details: "Sauna and solarium, a fitness corner, bicycle and Mikalto Walking poles available. To stay in shape especially on vaccation."
-    },
-    service3: {
-      url: "https://i.ibb.co/hZqy61V/location.jpg",
-      heading: "Perfect Location",
-      details: "The hotel is location near sone of the most beautiful sandy beaches. This is the hotel to book if you plan to spend a great deal of time.",
-    }
+  service2: {
+    url: "https://i.ibb.co/VqPmM5n/yoga.jpg",
+    heading: "Yoga & Gym",
+    details:
+      "Sauna and solarium, a fitness corner, bicycle and Mikalto Walking poles available. To stay in shape especially on vaccation.",
   },
-  reviews: {
+  service3: {
+    url: "https://i.ibb.co/hZqy61V/location.jpg",
+    heading: "Perfect Location",
+    details:
+      "The hotel is location near sone of the most beautiful sandy beaches. This is the hotel to book if you plan to spend a great deal of time.",
+  },
+};
+
+export const services = {
+  wellnessService: {
+    url: "https://i.ibb.co/xYCYXrH/wellness.jpg",
+    heading: "WELLNESS",
+    subHeading: "Dedicated to wellness",
+    details:
+      " Aman's approach to wellness is focused on personalisation , privacy and seclusion. Local spa rituals with indigenous ingredients, offer a profoundly therapeutic touch, and a gentle sense of discovery.",
+  },
+  giftCardService: {
+    url: "https://i.ibb.co/2NwHZBM/giftCard.jpg",
+    heading: "GIFT CARDS",
+    subHeading: "The gift of future travel",
+    details:
+      "Share extraordinary experiences with the Aman E-Gift Card - delivery instantly and offering memories that last a lifetime",
+  },
+
+  spaService: {
+    url: "https://i.ibb.co/7vjXM6D/spa.jpg",
+    heading: "MIKALTO SHOP",
+    subHeading: "The spa experience from home",
+    details:
+      "Skincare and spa supplements harness powerful natural ingredients to provide balance and optimal welbeing.",
+  },
+  adventureService: {
+    url: "https://i.ibb.co/Z8W61M1/adventure.jpg",
+    heading: "EXPERIENCE",
+    subHeading: "Adventure in the wild",
+    details:
+      "Skincare and spa supplements harness powerful natural ingredients to provide balance and optimal welbeing.",
+  },
+  service5: {
+    url: "https://i.ibb.co/xYCYXrH/wellness.jpg",
+    heading: "WELLNESS",
+    subHeading: "Dedicated to wellness",
+    details:
+      " Aman's approach to wellness is focused on personalisation , privacy and seclusion. Local spa rituals with indigenous ingredients, offer a profoundly therapeutic touch, and a gentle sense of discovery.",
+  },
+  service6: {
+    url: "https://i.ibb.co/2NwHZBM/giftCard.jpg",
+    heading: "GIFT CARDS",
+    subHeading: "The gift of future travel",
+    details:
+      "Share extraordinary experiences with the Aman E-Gift Card - delivery instantly and offering memories that last a lifetime",
+  },
+
+  service7: {
+    url: "https://i.ibb.co/7vjXM6D/spa.jpg",
+    heading: "MIKALTO SHOP",
+    subHeading: "The spa experience from home",
+    details:
+      "Skincare and spa supplements harness powerful natural ingredients to provide balance and optimal welbeing.",
+  },
+  service8: {
+    url: "https://i.ibb.co/Z8W61M1/adventure.jpg",
+    heading: "EXPERIENCE",
+    subHeading: "Adventure in the wild",
+    details:
+      "Skincare and spa supplements harness powerful natural ingredients to provide balance and optimal welbeing.",
+  },
+};
+
+export const reviews = {
+  review1: {
     logo: "https://cdn.logojoy.com/wp-content/uploads/20200605160841/tripadvisor-logo-.png",
-    review1: {
-      picture: "https://img.freepik.com/free-photo/happiness-wellbeing-confidence-concept-cheerful-attractive-african-american-woman-curly-haircut-cross-arms-chest-self-assured-powerful-pose-smiling-determined-wear-yellow-sweater_176420-35063.jpg",
-      name: "Selena Mayer",
-      date: "2021-01-10",
-      stars: 5,
-      recommendation: "Wonderful new year trip",
-      recommendationComment: "One of the best hotels I've ever stayed in. The staff are very friendly and helpful, they were so helpful it made me feel bad. It includes use of the Spa, wonderful, it has a..",
-    },
-    review2: {
-      picture: "https://media.istockphoto.com/id/1296158947/photo/portrait-of-creative-trendy-black-african-male-designer-laughing.jpg?s=612x612&w=0&k=20&c=1Ws_LSzWjYvegGxHYQkkgVytdpDcnmK0upJyGOzEPcg=",
-      name: "Esther Howard",
-      date: "2021-01-10",
-      stars: 4,
-      recommendation: "Recommended",
-      recommendationComment: "Great resort, professional staffs. It turns out that you're searching for paradise and you have it hear, nearby. Thanks for the warmth, you are fantastic.",
-    },
-    review3: {
-      picture: "https://www.rri.res.in/sites/default/files/2022-09/Abhisek%20Tamang.jpg",
-      name: "Jane Cooper",
-      date: "2021-01-10",
-      stars: 5,
-      recommendation: "All good",
-      recommendationComment: "I love the Resort and its environment. Mikalto is incredibly dedicated, knowledgeable, and helpful. The finished product was beautiful every penny...",
-    }
+    picture:
+      "https://img.freepik.com/free-photo/happiness-wellbeing-confidence-concept-cheerful-attractive-african-american-woman-curly-haircut-cross-arms-chest-self-assured-powerful-pose-smiling-determined-wear-yellow-sweater_176420-35063.jpg",
+    name: "Selena Mayer",
+    date: "2021-01-10",
+    stars: 5,
+    recommendation: "Wonderful new year trip",
+    recommendationComment:
+      "One of the best hotels I've ever stayed in. The staff are very friendly and helpful, they were so helpful it made me feel bad. It includes use of the Spa, wonderful, it has a..",
   },
-}
+  review2: {
+    logo: "https://cdn.logojoy.com/wp-content/uploads/20200605160841/tripadvisor-logo-.png",
+    picture:
+      "https://media.istockphoto.com/id/1296158947/photo/portrait-of-creative-trendy-black-african-male-designer-laughing.jpg?s=612x612&w=0&k=20&c=1Ws_LSzWjYvegGxHYQkkgVytdpDcnmK0upJyGOzEPcg=",
+    name: "Esther Howard",
+    date: "2021-01-10",
+    stars: 3,
+    recommendation: "Recommended",
+    recommendationComment:
+      "Great resort, professional staffs. It turns out that you're searching for paradise and you have it hear, nearby. Thanks for the warmth, you are fantastic.",
+  },
+  review3: {
+    logo: "https://cdn.logojoy.com/wp-content/uploads/20200605160841/tripadvisor-logo-.png",
+    picture:
+      "https://www.rri.res.in/sites/default/files/2022-09/Abhisek%20Tamang.jpg",
+    name: "Jane Cooper",
+    date: "2021-01-10",
+    stars: 5,
+    recommendation: "All good",
+    recommendationComment:
+      "I love the Resort and its environment. Mikalto is incredibly dedicated, knowledgeable, and helpful. The finished product was beautiful every penny...",
+  },
+};
+
+export const locationSection = {
+  flowerLogo: "https://i.ibb.co/VQ4Gg48/flower.jpg",
+  busLogo: "https://i.ibb.co/RjsmpTw/bus.jpg",
+  vanLogo: "https://i.ibb.co/1Q7JRFW/van.jpg",
+  planeLogo: "https://i.ibb.co/bJHw5qg/plane.jpg",
+  largeTravelPicture: "https://i.ibb.co/TW3WNt9/travel-Image1.jpg",
+  smallTravelPicture1: "https://i.ibb.co/S008SJS/travel-Image2.jpg",
+  smallTravelPicture2: "https://i.ibb.co/MgfV0Zd/travel-Image3.jpg",
+};
--- server.mjs
+++ server.mjs
@@ -6,31 +6,61 @@ import { parse } from "querystring";
 const port = 5000;
 
 function getRequestData(req) {
+  let status = 200;
   if (req.url === "/") {
-    let status = 200;
+    return JSON.stringify({
+      status,
+      text : "Welcome to Rest API"
+    });
+  } else if (req.url === "/page-banner") {
     return JSON.stringify({
       status,
       pageBanner: data.pageBanner,
-      homestays: data.homestays,
-      locationSection: data.locationSection,
-      activities: data.activities,
-      servicesBanner: data.servicesBanner,
-      wellnessService: data.wellnessService,
-      giftCardService: data.giftCardService,
-      spaService: data.spaService,
-      adventureService: data.adventureService,
-      showRooms: data.showRooms,
-      text: data.text,
     });
-  } else {
+  } else if (req.url === "/homestays") {
+    return JSON.stringify({
+      status,
+      homestays: data.homestays,
+    });
+  } else if (req.url === "/show-rooms") {
+    return JSON.stringify({
+      status,
+      showRooms: data.showRooms,
+    });
+  }
+  else if (req.url === "/our-hotel-services"){
+    return JSON.stringify({
+      status,
+      ourHostelServices : data.ourHostelServices,
+    })
+  } 
+  else if (req.url === "/services"){
+    return JSON.stringify({
+      status,
+      services : data.services,
+    })
+  } 
+  else if (req.url === "/reviews"){
+    return JSON.stringify({
+      status,
+      reviews : data.reviews,
+    })
+  } 
+  else if (req.url === "/location"){
+    return JSON.stringify({
+      status,
+      location : data.locationSection,
+    })
+  } 
+  else {
     return JSON.stringify("Page not available.");
   }
 }
 
 async function writeFormData(formData) {
   const dataFromFile = await fs.readFile("./formData.txt", "utf8");
-  const fileData = JSON.parse(dataFromFile)
-  fileData.push(formData)
+  const fileData = JSON.parse(dataFromFile);
+  fileData.push(formData);
   await fs.writeFile("./formData.txt", JSON.stringify(fileData));
 }
 
@@ -44,7 +74,7 @@ const server = http.createServer((req, res) => {
     });
     req.on("end", () => {
       if (Object.keys(formData).length !== 0) {
-        writeFormData(parse(formData))
+        writeFormData(parse(formData));
       }
     });
     res.end(getRequestData(req));
